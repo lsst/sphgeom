ca9236b10843f16daa25d76bfc9d76780f56855c
Jim Bosch <jbosch@astro.princeton.edu>
2023-07-29 23:17:21 -0400

    Switch pybind11 usage to reflect smart_holder branch.
    Passing shared_ptr or unique_ptr as another template argument to
    pybind11::class_ is no longer necessary or allowed; both pointer types
    can now be used with all classes.

--- python/lsst/sphgeom/_compoundRegion.cc
+++ python/lsst/sphgeom/_compoundRegion.cc
@@ -91,7 +91,7 @@ private:
 }  // namespace
 
 template <>
-void defineClass(py::class_<CompoundRegion, std::unique_ptr<CompoundRegion>, Region> &cls) {
+void defineClass(py::class_<CompoundRegion, Region> &cls) {
     cls.def("nOperands", &CompoundRegion::nOperands);
     cls.def("__len__", &CompoundRegion::nOperands);
     cls.def(
@@ -113,7 +113,7 @@ void defineClass(py::class_<CompoundRegion, std::unique_ptr<CompoundRegion>, Reg
 }
 
 template <>
-void defineClass(py::class_<UnionRegion, std::unique_ptr<UnionRegion>, CompoundRegion> &cls) {
+void defineClass(py::class_<UnionRegion, CompoundRegion> &cls) {
     cls.attr("TYPE_CODE") = py::int_(UnionRegion::TYPE_CODE);
     cls.def(py::init(&_args_factory<UnionRegion>));
     cls.def(py::pickle(&python::encode, &python::decode<UnionRegion>));
@@ -121,7 +121,7 @@ void defineClass(py::class_<UnionRegion, std::unique_ptr<UnionRegion>, CompoundR
 }
 
 template <>
-void defineClass(py::class_<IntersectionRegion, std::unique_ptr<IntersectionRegion>, CompoundRegion> &cls) {
+void defineClass(py::class_<IntersectionRegion, CompoundRegion> &cls) {
     cls.attr("TYPE_CODE") = py::int_(IntersectionRegion::TYPE_CODE);
     cls.def(py::init(&_args_factory<IntersectionRegion>));
     cls.def(py::pickle(&python::encode, &python::decode<IntersectionRegion>));
--- python/lsst/sphgeom/_lonLat.cc
+++ python/lsst/sphgeom/_lonLat.cc
@@ -40,7 +40,7 @@ namespace lsst {
 namespace sphgeom {
 
 template <>
-void defineClass(py::class_<LonLat, std::shared_ptr<LonLat>> &cls) {
+void defineClass(py::class_<LonLat> &cls) {
     cls.def_static("fromDegrees", &LonLat::fromDegrees);
     cls.def_static("fromRadians", &LonLat::fromRadians);
     cls.def_static("latitudeOf", &LonLat::latitudeOf);
