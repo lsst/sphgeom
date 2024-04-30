eb7e0cfc7c0e05876a81be0e3c0d456aebb60c4e
Matthias Wittgen <Matthias.Michael.Wittgen@cern.ch>
2024-04-30 15:17:23 -0700

    Fix unit test

--- tests/test_CompoundRegion.py
+++ tests/test_CompoundRegion.py
@@ -79,7 +79,7 @@ class CompoundRegionTestMixin:
         """Assert that a compound regions operands are equal to the given
         tuple of operands.
         """
-        self.assertCountEqual((region.cloneOperand(0), region.cloneOperand(1)), operands)
+        self.assertEqual((region.cloneOperand(0), region.cloneOperand(1)), operands)
 
     def assertCompoundRegionsEqual(self, a, b):
         """Assert that two compound regions are equal.
@@ -131,7 +131,6 @@ class CompoundRegionTestMixin:
         """Test that repr returns a string that can be eval'd to yield an
         equivalent instance.
         """
-        print(repr(self.instance))
         self.assertCompoundRegionsEqual(
             self.instance,
             eval(
